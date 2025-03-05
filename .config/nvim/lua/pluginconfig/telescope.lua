9a4719d398b3e1575a89d8436000e9a10757a593
miura <shinji65536@gmail.com>
2025-03-05 11:40:05 +0900

    fix-telescope: git管理されている隠しファイルも検索対象とする

--- .config/nvim/lua/pluginconfig/telescope.lua
+++ .config/nvim/lua/pluginconfig/telescope.lua
@@ -1,25 +1,42 @@
 local actions = require("telescope.actions")
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
 
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-v>"] = actions.select_horizontal,
-        ["<C-s>"] = actions.select_vertical,
-      },
-    },
-  },
-  extensions = {
-    fzf = {
-      fuzzy = true,
-      override_generic_sorter = true,
-      override_file_sorter = true,
-      case_mode = "smart_case",
-    }
-  }
-}
-require('telescope').load_extension('fzf')
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
 
-local builtin = require('telescope.builtin')
-vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = 'Find files by Telescope' })
-vim.keymap.set("n", "<C-h>", builtin.live_grep, { desc = 'Grep files by Telescope' })
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+require("telescope").setup({
+	defaults = {
+		vimgrep_arguments = vimgrep_arguments,
+		mappings = {
+			i = {
+				["<C-v>"] = actions.select_horizontal,
+				["<C-s>"] = actions.select_vertical,
+			},
+		},
+	},
+	pickers = {
+		find_files = {
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true,
+			override_file_sorter = true,
+			case_mode = "smart_case",
+		},
+	},
+})
+require("telescope").load_extension("fzf")
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files by Telescope" })
+vim.keymap.set("n", "<C-h>", builtin.live_grep, { desc = "Grep files by Telescope" })
