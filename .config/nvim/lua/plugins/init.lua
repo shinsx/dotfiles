fdae15c1a4fc2f599e4e7b7ecf4612b9c05840fe
miura <shinji65536@gmail.com>
2025-05-18 20:23:34 +0900

    add-plugin: which-key

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -368,11 +368,24 @@ return {
 			require("pluginconfig/toggleterm")
 		end,
 	},
-	-- {
-	--   'vim-denops/denops.vim',
-	--   event = "VimEnter",
-	--   dependencies = {
-	--     'tani/vim-glance',
-	--   },
+  {
+		-- checkhealth which-key で干渉しているkeymapを表示
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  }
 }
 -- opt = trueをつければsetupだけの設定ファイルを削除する
