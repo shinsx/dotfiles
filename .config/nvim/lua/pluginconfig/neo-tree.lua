8a54de0db38cf5578a2bcb641c4d494a9732c833
miura <shinji65536@gmail.com>
2025-06-07 11:59:04 +0900

    fix: net-treeのアイコンを変更

--- .config/nvim/lua/pluginconfig/neo-tree.lua
+++ .config/nvim/lua/pluginconfig/neo-tree.lua
@@ -62,15 +62,13 @@ require("neo-tree").setup({
         },
         git_status = {
             symbols = {
-                -- Change type
-                added     = "✚", -- or "", but this is redundant info if you use git_status_colors on the name
-                modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-                deleted   = "✖", -- this can only be used in the git_status source
-                renamed   = "", -- this can only be used in the git_status source
-                -- Status type
+                added     = "✚",
+                deleted   = "✖",
+                modified  = "",
+                renamed   = "󰁕",
                 untracked = "",
                 ignored   = "",
-                unstaged  = "",
+                unstaged  = "󰄱",
                 staged    = "",
                 conflict  = "",
             }
