558f4d0df059915133002605f597f921c1449132
miura <shinji65536@gmail.com>
2026-01-11 09:39:37 +0900

    feat: mcp

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -121,6 +121,17 @@ return {
          },
       },
    },
+   {
+      "ravitemer/mcphub.nvim",
+      event = "VimEnter",
+      dependencies = {
+         "nvim-lua/plenary.nvim",
+      },
+      build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
+      config = function()
+         require("mcphub").setup()
+      end
+   },
    -- ==============LSP==============
    {
       "nvimtools/none-ls.nvim",
