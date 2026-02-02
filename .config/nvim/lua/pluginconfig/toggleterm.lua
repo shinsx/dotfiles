9cad5823eb6397d36cd00eaaab5ae66fe98ca356
miura <shinji65536@gmail.com>
2026-02-02 22:34:13 +0900

    feat: add tftui

--- .config/nvim/lua/pluginconfig/toggleterm.lua
+++ .config/nvim/lua/pluginconfig/toggleterm.lua
@@ -4,9 +4,19 @@ local lazygit = Terminal:new({
   direction = "float",
   hidden = true
 })
-
 function _lazygit_toggle()
   lazygit:toggle()
 end
 
+local tftui = Terminal:new({
+  cmd = "tftui",
+  direction = "float",
+  hidden = true
+})
+
+function _tftui_toggle()
+  tftui:toggle()
+end
+
 vim.api.nvim_set_keymap("n", "<space>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>tui", "<cmd>lua _tftui_toggle()<CR>", { noremap = true })
