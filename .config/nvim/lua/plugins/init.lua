b1f4fb31d7977c9ad0442aa87729cb447caa0d1f
miura <shinji65536@gmail.com>
2026-03-11 10:13:57 +0900

    fix(plugin): remove avante.nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -48,86 +48,12 @@ return {
       opts = {},
    },
    {
-      "yetone/avante.nvim",
-      event = "VeryLazy",
-      lazy = false,
-      version = false,
-      opts = {
-         provider = "copilot",
-         auto_suggestions_provider = "copilot",
-
-         behaviour = {
-            auto_suggestions = false,
-            auto_set_highlight_group = true,
-            auto_set_keymaps = true,
-            auto_apply_diff_after_generation = false,
-            support_paste_from_clipboard = false,
-            minimize_diff = true,
-         },
-
-         windows = {
-            position = "right",
-            wrap = true,
-            width = 30,
-            sidebar_header = {
-               enabled = true,
-               align = "center",
-               rounded = true,
-            },
-            input = {
-               prefix = "> ",
-               height = 8,
-            },
-            edit = {
-               border = "rounded",
-               start_insert = true,
-            },
-            ask = {
-               floating = false,
-               start_insert = true,
-               border = "rounded",
-               focus_on_apply = "ours",
-            },
-         },
-      },
-      build = "make",
-      dependencies = {
-         "stevearc/dressing.nvim",
-         "nvim-lua/plenary.nvim",
-         "MunifTanjim/nui.nvim",
-         "hrsh7th/nvim-cmp",
-         "nvim-tree/nvim-web-devicons",
-         "zbirenbaum/copilot.lua",
-         {
-            "HakonHarnes/img-clip.nvim",
-            event = "VeryLazy",
-            opts = {
-               default = {
-                  embed_image_as_base64 = false,
-                  prompt_for_file_name = false,
-                  drag_and_drop = {
-                     insert_mode = true,
-                  },
-                  use_absolute_path = true,
-               },
-            },
-         },
-         {
-            "MeanderingProgrammer/render-markdown.nvim",
-            opts = {
-               file_types = { "markdown", "Avante" },
-            },
-            ft = { "markdown", "Avante" },
-         },
-      },
-   },
-   {
       "ravitemer/mcphub.nvim",
       event = "VimEnter",
       dependencies = {
          "nvim-lua/plenary.nvim",
       },
-      build = "npm install -g mcp-hub@latest",  -- Installs `mcp-hub` node binary globally
+      build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
       config = function()
          require("mcphub").setup()
       end
