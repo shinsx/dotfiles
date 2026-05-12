25d97f3da2705322bbcba872c2026d261cc2b465
miura <shinji65536@gmail.com>
2026-05-12 10:57:04 +0900

    fix: fix: update nvim-treesitter config to new main branch API

--- .config/nvim/lua/pluginconfig/nvim-treesitter.lua
+++ .config/nvim/lua/pluginconfig/nvim-treesitter.lua
@@ -1,10 +1,6 @@
--- パーサーのインストール
-require('nvim-treesitter.configs').setup {
-   modules = {},
-   sync_install = false,
+-- パーサーのインストール（新 API: main ブランチ対応）
+require('nvim-treesitter').setup {
    auto_install = true, -- trueにすると未インストールの言語を開いた時に自動インストールされます
-   ignore_install = {},
-   -- パーサーのインストール設定
    ensure_installed = {
       "vim", "lua", "javascript", "typescript", "tsx", "json", "yaml", "toml",
       "html", "css", "scss", "ruby", "python", "rust", "go", "gomod", "gosum",
@@ -12,62 +8,56 @@ require('nvim-treesitter.configs').setup {
       "markdown_inline", "terraform", "nix", "nginx", "apache",
       "proto", "sql", "regex", "helm",
    },
+}
 
-   -- 同時にハイライトもここで有効化するのが一般的です
-   highlight = {
+-- textobjects の設定（nvim-treesitter-textobjects 独自の setup）
+require('nvim-treesitter-textobjects').setup {
+   select = {
       enable = true,
+      lookahead = true,
+      keymaps = {
+         ["af"] = "@function.outer",
+         ["if"] = "@function.inner",
+         ["ac"] = "@class.outer",
+         ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+      },
    },
-
-   -- textobjects の設定も setup 関数の中に含めるのが推奨される構成です
-   textobjects = {
-      select = {
-         enable = true,
-         lookahead = true,
-         keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-         },
-         -- ...（以下、元の textobjects 設定をここに統合できます）
+   swap = {
+      enable = true,
+      swap_next = {
+         ["<leader>a"] = "@parameter.inner",
       },
-      swap = {
-         enable = true,
-         swap_next = {
-            ["<leader>a"] = "@parameter.inner",
-         },
-         swap_previous = {
-            ["<leader>A"] = "@parameter.inner",
-         },
+      swap_previous = {
+         ["<leader>A"] = "@parameter.inner",
       },
-      move = {
-         enable = true,
-         set_jumps = true,
-         goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]]"] = "@class.outer",
-         },
-         goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]["] = "@class.outer",
-         },
-         goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[["] = "@class.outer",
-         },
-         goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[]"] = "@class.outer",
-         },
+   },
+   move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+         ["]m"] = "@function.outer",
+         ["]]"] = "@class.outer",
       },
-      lsp_interop = {
-         enable = true,
-         border = "none",
-         floating_preview_opts = {},
-         peek_definition_code = {
-            ["<leader>df"] = "@function.outer",
-            ["<leader>dF"] = "@class.outer",
-         },
+      goto_next_end = {
+         ["]M"] = "@function.outer",
+         ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+         ["[m"] = "@function.outer",
+         ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+         ["[M"] = "@function.outer",
+         ["[]"] = "@class.outer",
+      },
+   },
+   lsp_interop = {
+      enable = true,
+      border = "none",
+      floating_preview_opts = {},
+      peek_definition_code = {
+         ["<leader>df"] = "@function.outer",
+         ["<leader>dF"] = "@class.outer",
       },
    },
 }
