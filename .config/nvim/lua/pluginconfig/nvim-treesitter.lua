92c541adade2bff74891eeafe284fc178d578649
miura <shinji65536@gmail.com>
2026-05-26 09:25:11 +0900

    fix: textobjectsの不要なsetupを削除

--- .config/nvim/lua/pluginconfig/nvim-treesitter.lua
+++ .config/nvim/lua/pluginconfig/nvim-treesitter.lua
@@ -10,58 +10,6 @@ require('nvim-treesitter').setup {
    },
 }
 
--- textobjects の設定（nvim-treesitter-textobjects 独自の setup）
-require('nvim-treesitter-textobjects').setup {
-   select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-         ["af"] = "@function.outer",
-         ["if"] = "@function.inner",
-         ["ac"] = "@class.outer",
-         ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-      },
-   },
-   swap = {
-      enable = true,
-      swap_next = {
-         ["<leader>a"] = "@parameter.inner",
-      },
-      swap_previous = {
-         ["<leader>A"] = "@parameter.inner",
-      },
-   },
-   move = {
-      enable = true,
-      set_jumps = true,
-      goto_next_start = {
-         ["]m"] = "@function.outer",
-         ["]]"] = "@class.outer",
-      },
-      goto_next_end = {
-         ["]M"] = "@function.outer",
-         ["]["] = "@class.outer",
-      },
-      goto_previous_start = {
-         ["[m"] = "@function.outer",
-         ["[["] = "@class.outer",
-      },
-      goto_previous_end = {
-         ["[M"] = "@function.outer",
-         ["[]"] = "@class.outer",
-      },
-   },
-   lsp_interop = {
-      enable = true,
-      border = "none",
-      floating_preview_opts = {},
-      peek_definition_code = {
-         ["<leader>df"] = "@function.outer",
-         ["<leader>dF"] = "@class.outer",
-      },
-   },
-}
-
 -- FileType ごとにハイライトを有効化
 vim.api.nvim_create_autocmd("FileType", {
    callback = function()
